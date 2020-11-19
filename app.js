2a90a58c47beb5333d34f77b6868a6bfb40858f4
jroutledge <jroulted@ualberta.ca>
2020-11-18 20:16:26 -0700

    Added app.js with stubs and added .gitignore

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.json({ extended: false }));
+
+app.get('/', (req, res) => {
+    //TODO implement main app route
+});
+
+app.get('/location', (req, res) => {
+    const location = +req.query.location;
+    //TODO get data for specific location id (will this be a token or something?)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
