f8c9774218ddd6107b8f61e80f7c45c08623f285
jeffroutledge <jroutledge7@gmail.com>
2021-05-24 14:39:21 -0600

    Removed commented code and some debugging console logs.

--- controllers/locationController.ts
+++ controllers/locationController.ts
@@ -19,14 +19,6 @@ class LocationController {
             const location = req?.query?.location?.toString() ?? 'canada';
             const covidStats = await new Covid19Service().getCovidStatsByCountry(req, res, location);
             res.status(200).json(covidStats);
-            // const covid19Service = new Covid19Service();
-            // await covid19Service.getCovidStatsByCountry(req, res, location)
-            //     .then((covidStats) => {
-            //         console.log(covidStats);
-            //         res.status(200).json(covidStats);
-            //     });
-            // console.log("here: ", result.then);
-            // res.send(result);
         } catch (e) {
             console.log(e.message);
             res.status(500).json(e);
--- services/Covid19Service.ts
+++ services/Covid19Service.ts
@@ -5,11 +5,8 @@ import axios from 'axios';
 class Covid19Service {
     public async getCovidStatsByCountry (req: express.Request, res: express.Response, location: string): Promise<any>
     {
-        console.log('we got the covid19 service working');
-
         const config = {
             baseURL: 'https://api.covid19api.com',
-            // path: '/country/canada/status/confirmed',
             url: `/dayone/country/${location}/status/confirmed/live`,
             timeout: 1000,
             headers: {
@@ -18,38 +15,9 @@ class Covid19Service {
             }
         };
 
-        // console.log(config.baseURL + config.url);
-
         let result = await axios.get(`/dayone/country/${location}/status/confirmed/live`, config)
 
         return result.data;
-            // .then((response) => {
-            //     // console.log(response.data);
-            //     return response;
-            //     // console.log(response.status);
-            //     // console.log(response.statusText);
-            //     // console.log(response.headers);
-            //     // console.log(response.config);
-            //     // return response.data;
-            // })
-            // .catch((e) => {
-            //     return e;
-            // })
-
-        // const callback = async (response: any) => {
-        // response.on('data', (chunk: any) => {
-        //     result += chunk;
-        // });
-
-        // response.on('end', () => {
-        //     res.status(200).json(result); // SEND ACTUAL RESPONSE HERE
-        // });
-        // }
-
-        // req = http.request(options, callback);
-        // req.end();
-        console.log("done in service: ", response);
-        // return response.data;
     }
 }
 
