bd5696bf01438db0bbe89d1dacb4fdb18c343160
jeffroutledge <jroutledge7@gmail.com>
2021-05-25 13:11:16 -0600

    Updated linter got client running.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React feat the boys
         </a>
       </header>
     </div>
