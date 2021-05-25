8947fa341e080f116e842b2898208ede42100c97
Dexter Harvey <74616615+DexterHarvey@users.noreply.github.com>
2021-05-25 15:43:20 -0600

    Wired up front end to consume World endpoint, and set up recharts
    Co-authored-by: jeffroutledge <jeffroutledge@users.noreply.github.com>

--- client/src/TestComponent.tsx
+++ client/src/TestComponent.tsx
@@ -1,33 +1,18 @@
-// import {Component} from 'react';
-// import axios from 'axios';
+import {Component} from 'react';
+//import axios from 'axios';
 
-export default class TestComponent{}
-// export default class TestComponent extends Component {
-//     state = {
-//         externalData: null,
-//     };
-//     private _asyncRequest: string; 
-
-//     componentWillMount() {
-//         this._asyncRequest = loadMyAsyncData().then(
-//             externalData => {
-//                 this._asyncRequest = null;
-//                 this.setState({ externalData });
-//             }
-//         );
-//     }  
-
-//     componentWillUnmount() {
-//         if (this._asyncRequest) {
-//         this._asyncRequest.cancel();
-//         }
-//     }
-
-//     render() {
-//         if (this.state.externalData === null) {
-//          return (<div>Data not yet loaded</div>)
-//         } else {
-//             return (<div></div>)
-//     }
-//   }
-// }
+export default class TestComponent extends Component<{}, { world: string}>{
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            world: "null"
+        }
+    }
+    render() {
+        return (
+            <div>
+                <p>{this.state.world}</p>
+            </div>
+        )
+    }
+}
