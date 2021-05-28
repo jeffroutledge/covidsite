b1e4059bfadf9f31f50145822737a651615a4d55
Dexter Harvey <74616615+DexterHarvey@users.noreply.github.com>
2021-05-28 17:44:26 -0600

    Implemented reverse geolocation components on back end

--- client/src/ProvStats.tsx
+++ client/src/ProvStats.tsx
@@ -1,42 +1,47 @@
 import { Component } from 'react';
-import TerritoryStats from './TerritoryStats';
+import TerritoryStats from './Models/TerritoryStats';
 
 export default class ProvStats extends Component<{}, {territoryStats: TerritoryStats}> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            lat = this.props.latitude;
+            lon = this.props.longitude;
+        }
+    }
     componentDidMount() {
         this.setState({...this.state});
-        try {
-            fetch('http://localhost:8080/location')
-                .then(response => response.json())
-                .then(result => {
-                    console.log(result['features'][0]['attributes']['NAME']);
-                    const tStats: TerritoryStats = {
-                        ObjectID : result['features'][0]['attributes']['OBJECTID'],
-                        Name : result['features'][0]['attributes']['NAME'],
-                        Recovered : result['features'][0]['attributes']['Recovered'],
-                        Tests : result['features'][0]['attributes']['Tests'],
-                        LastUpdated : result['features'][0]['attributes']['Last_Updated'],
-                        SourceURL : result['features'][0]['attributes']['SourceURL'],
-                        ActiveCases : result['features'][0]['attributes']['ActiveCases'],
-                        Hospitalized : result['features'][0]['attributes']['Hospitalized'],
-                        ICU : result['features'][0]['attributes']['ICU'],
-                        ICUVentilator : result['features'][0]['attributes']['ICUVents'],
-                        Deaths : result['features'][0]['attributes']['Deaths'],
-                        CaseTotal : result['features'][0]['attributes']['Case_Total'],
-                        Vaccinated : result['features'][0]['attributes']['Vaccinated'],
-                    }
-                    this.setState({territoryStats: tStats});
-                })
-                .catch(e => {
-                    console.log(e);
-                    this.setState({...this.state});
-                });
+    try {
+        fetch(`http://localhost:8080/location?longitude=49.183&latitude=-122.868`)
+            .then(response => response.json())
+            .then(result => {
+                const tStats: TerritoryStats = {
+                    ObjectID : result['features'][0]['attributes']['OBJECTID'],
+                    Name : result['features'][0]['attributes']['NAME'],
+                    Recovered : result['features'][0]['attributes']['Recovered'],
+                    Tests : result['features'][0]['attributes']['Tests'],
+                    LastUpdated : result['features'][0]['attributes']['Last_Updated'],
+                    SourceURL : result['features'][0]['attributes']['SourceURL'],
+                    ActiveCases : result['features'][0]['attributes']['ActiveCases'],
+                    Hospitalized : result['features'][0]['attributes']['Hospitalized'],
+                    ICU : result['features'][0]['attributes']['ICU'],
+                    ICUVentilator : result['features'][0]['attributes']['ICUVents'],
+                    Deaths : result['features'][0]['attributes']['Deaths'],
+                    CaseTotal : result['features'][0]['attributes']['Case_Total'],
+                    Vaccinated : result['features'][0]['attributes']['Vaccinated'],
+                }
+                this.setState({territoryStats: tStats});
+            })
+            .catch(e => {
+                console.log(e);
+                this.setState({...this.state});
+            });
         }
         catch (e) {
             console.log(e);
         }
     }
     
-
     render() {
         let renderProvJSON;
         try
