913e8bdbbb5dc722e74d4282307d469380b9efe1
Dexter Harvey <74616615+DexterHarvey@users.noreply.github.com>
2021-05-28 09:26:22 -0600

    changed App.tsx to class from function to implement geolocation, implemented geolocation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,28 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 import World from './World';
 import ProvStats from './ProvStats';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-         <World />
-         <ProvStats />
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        console.log("Latitude is :", position.coords.latitude);
+        console.log("Longitude is :", position.coords.longitude);
+    });
+    }
+    render(): any {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                    Learn React
+                    </a>
+                    <World />
+                    <ProvStats />
+                </header>
+            </div>
+        )
+    }
 }
-
+render(<App />, document.getElementById('root'));
 export default App;
