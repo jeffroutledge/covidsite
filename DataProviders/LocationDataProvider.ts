02d528e884c81ec681303b1b6a6ecf182429d15e
jeffroutledge <jroutledge7@gmail.com>
2021-05-30 12:04:07 -0600

    Finished implementing call to get data off lat lon coordinates.

--- DataProviders/LocationDataProvider.ts
+++ DataProviders/LocationDataProvider.ts
@@ -4,11 +4,13 @@ import Location from '../Models/Location'
 class LocationDataProvider {
     private config = {
         baseURL: 'https://api.mapbox.com',
-        timeout: 1000,
+        timeout: 2000,
     };
 
     public async getLocationDataFromCoordinates(location : Location) {
-        const result = await axios.get(`/geocoding/v5/mapbox.places/${location.Latitude},${location.Longitude}.json?access_token=pk.eyJ1IjoiZGV4dGVyaGFydmV5IiwiYSI6ImNrcDhtY2p1dDBhMHIyd28xNHVnaXVxMjMifQ.sQgjJ-2E64Kz9zw2mfv-Kw`, this.config);
+        const url = `/geocoding/v5/mapbox.places/${location.Longitude},${location.Latitude}.json?access_token=pk.eyJ1IjoiZGV4dGVyaGFydmV5IiwiYSI6ImNrcDhtY2p1dDBhMHIyd28xNHVnaXVxMjMifQ.sQgjJ-2E64Kz9zw2mfv-Kw`;
+        console.log(url);
+        const result = await axios.get(url, this.config);
         return result.data;
     }
 }
