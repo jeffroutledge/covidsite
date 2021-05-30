f555ae5fbf6b55a55c9a374b8ba9f2d94d821414
jeffroutledge <jroutledge7@gmail.com>
2021-05-30 12:04:42 -0600

    Removed commented code.

--- client/src/ProvStats.tsx
+++ client/src/ProvStats.tsx
@@ -6,63 +6,6 @@ export default class ProvStats extends Component<{longitude: any, latitude: any}
     private latitude: number = 0;
     private longitude: number = 0;
 
-    // constructor(props: any) {
-    //     super(props);
-    //     navigator.geolocation.getCurrentPosition(async (position) => {
-    //         this.latitude = position.coords.latitude;
-    //         this.longitude = position.coords.longitude;
-    //         console.log(this.latitude, position.coords.latitude)
-    //         this.setState(await this.getLocationFromPosition(position.coords.latitude, position.coords.longitude));
-    //     });
-    // }
-
-    // constructor(props: any) {
-    //     super(props);
-
-    //     navigator.geolocation.getCurrentPosition(async (position) => {
-    //         this.latitude = position.coords.latitude;
-    //         this.longitude = position.coords.longitude;
-    //         console.log(this.latitude, position.coords.latitude)
-    //         this.setState(await this.getLocationFromPosition(position.coords.latitude, position.coords.longitude));
-    //     });
-    // }
-    // constructor(props: any) {
-    //     super(props);
-    //     try {
-    //         fetch(`http://localhost:8080/location?longitude=${this.props.longitude}&latitude=${this.props.latitude}`)
-    //         // fetch(`http://localhost:8080/location?longitude=49.183&latitude=-122.868`)
-    //             .then(response => response.json())
-    //             .then(result => {
-    //                 const tStats: TerritoryStats = {
-    //                     ObjectID : result['features'][0]['attributes']['OBJECTID'],
-    //                     Name : result['features'][0]['attributes']['NAME'],
-    //                     Recovered : result['features'][0]['attributes']['Recovered'],
-    //                     Tests : result['features'][0]['attributes']['Tests'],
-    //                     LastUpdated : result['features'][0]['attributes']['Last_Updated'],
-    //                     SourceURL : result['features'][0]['attributes']['SourceURL'],
-    //                     ActiveCases : result['features'][0]['attributes']['ActiveCases'],
-    //                     Hospitalized : result['features'][0]['attributes']['Hospitalized'],
-    //                     ICU : result['features'][0]['attributes']['ICU'],
-    //                     ICUVentilator : result['features'][0]['attributes']['ICUVents'],
-    //                     Deaths : result['features'][0]['attributes']['Deaths'],
-    //                     CaseTotal : result['features'][0]['attributes']['Case_Total'],
-    //                     Vaccinated : result['features'][0]['attributes']['Vaccinated'],
-    //                 }
-    //                 this.setState({territoryStats: tStats});
-    //             })
-    //             .catch(e => {
-    //                 console.log(e);
-    //                 this.setState({...this.state});
-    //             });
-    //         }
-    //         catch (e) {
-    //             console.log(e);
-    //         }
-    //     // this.state = {
-    //     //     latituade = this.props.latitude;
-    //     //     longitude = this.props.longitude;
-    //     // }
-    // }
     componentDidMount() {
         navigator.geolocation.getCurrentPosition(async (position) => {
             this.latitude = position.coords.latitude;
