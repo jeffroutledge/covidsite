74c38e46c2680322b10d6139176cc9087a63b920
jeffroutledge <jroutledge7@gmail.com>
2021-05-30 12:04:07 -0600

    Finished implementing call to get data off lat lon coordinates.

--- controllers/locationController.ts
+++ controllers/locationController.ts
@@ -3,6 +3,7 @@ import Covid19Service from '../services/Covid19Service';
 import LocationService from '../services/LocationService';
 import Controller from './controller.interface';
 import Location from '../Models/Location';
+import TerritoryStats from '../Models/TerritoryStats';
 
 class LocationController implements Controller{
     public path = '/location';
@@ -30,12 +31,28 @@ class LocationController implements Controller{
                     Longitude : querylongitude,
                     Latitude : querylatitude
                 };
-                let reverseGeocodedData = await new LocationService().getLocationByCoordinates(location);
-                location.City = reverseGeocodedData['features'][2]['text'];
-                location.Territory = (reverseGeocodedData['features'][3]['text']).toUpperCase();
-                location.Country = reverseGeocodedData['features'][4]['text'];
+                let reverseGeocodedData: any[] = (await new LocationService().getLocationByCoordinates(location)).features;
+                //potentially use place_type[0] === place instead of the id.includes
+                location.City = reverseGeocodedData.filter((i: { id: string; }) => i.id.includes('place'))[0].text;
+                location.Territory = (reverseGeocodedData.filter((i: {id: string;}) => i.id.includes('region'))[0].text).toUpperCase();
+                location.Country = reverseGeocodedData.filter((i: {id: string;}) => i.id.includes('country'))[0].text;
                 if (location.Country == 'Canada'){
-                        const covidStats = await new Covid19Service().getCovidStatsByCanadaProvTerritory(location);
+                        const result  = await new Covid19Service().getCovidStatsByCanadaProvTerritory(location);
+                        const covidStats: TerritoryStats = {
+                            ObjectID : result['features'][0]['attributes']['OBJECTID'],
+                            Name : result['features'][0]['attributes']['NAME'],
+                            Recovered : result['features'][0]['attributes']['Recovered'],
+                            Tests : result['features'][0]['attributes']['Tests'],
+                            LastUpdated : result['features'][0]['attributes']['Last_Updated'],
+                            SourceURL : result['features'][0]['attributes']['SourceURL'],
+                            ActiveCases : result['features'][0]['attributes']['ActiveCases'],
+                            Hospitalized : result['features'][0]['attributes']['Hospitalized'],
+                            ICU : result['features'][0]['attributes']['ICU'],
+                            ICUVentilator : result['features'][0]['attributes']['ICUVents'],
+                            Deaths : result['features'][0]['attributes']['Deaths'],
+                            CaseTotal : result['features'][0]['attributes']['Case_Total'],
+                            Vaccinated : result['features'][0]['attributes']['Vaccinated'],
+                        }
                         res.status(200).json(covidStats);
                 }
             }
