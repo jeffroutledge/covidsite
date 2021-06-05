71b9f3ce3ff7f6fceeec722bf71bd4f48b32aef2
jeffroutledge <jroutledge7@gmail.com>
2021-06-05 15:37:04 -0600

    Testing if environment is set to prod.

--- App.ts
+++ App.ts
@@ -29,14 +29,14 @@ class App {
       }
 
       private initializeHome() {
-        if (process.env.NODE_ENV === 'production') {
+        console.log("env", process.env.NODE_ENV);
+        // if (process.env.NODE_ENV === 'production') {
           this.app.use(express.static(path.join(__dirname, 'client/build')));
       
-          this.app.get('*', function (req, res) {
-            console.log(req.baseUrl);
+          this.app.get('*', function (_req, res) {
             res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
           });
-        }
+        // }
       }
 
       public listen() {
